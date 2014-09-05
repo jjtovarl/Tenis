1753ff05987de5724d2c81ca1854d390dc599032
jjtovarl <jtovarl13@hotmail.com>
2014-09-04 23:20:27 -0500

    Update Tenis.py

--- Tenis.py
+++ Tenis.py
@@ -1,182 +1,51 @@
-def score():
-  """
-  Inicio del partido, cuando los marcadores son 0
-  Por ejemplo:
-  >>> score()
-  (0, 0)
-  """
-  j1 = 0
-  j2 = 0
-  
-  return j1,j2
+"""
+>>> D(0,0).score()
+[0, 0]
+>>> D(1,0).score()
+[15, 0]
+>>> D(2,0).score()
+[30, 0]
+>>> D(3,0).score()
+[40, 0]
+>>> D(0,1).score()
+[0, 15]
+>>> D(1,2).score()
+[15, 30]
+>>> D(2,2).score()
+[30, 30]
+>>> D(2,3).score()
+[30, 40]
+>>> D(3,3).score()
+['Douce']
+>>> D(4,3).score()
+['Adv', 40]
+>>> D(3,4).score()
+[40, 'Adv']
+>>> D(4,4).score()
+['Douce']
+>>> D(5,4).score()
+['Game', 'less']
+>>> D(4,5).score()
+['less', 'Game']
+"""
+class D:
+	scores = [0, 15, 30, 40,'Adv','Game']
 
-def score1(j):
-  """
-  El jugador 1 inicia con el saque y no es interceptado por el jugador 2, entonces anota
-  Por ejemplo:
-  >>> score1(1)
-  (15, 0)
-  """
-  j1 = 15
-  j2 = 0
+	def __init__(self, j1, j2):
+		self.j1 = j1
+		self.j2 = j2
 
-  return j1,j2
-
-def score2(j):
-  """
-  El jugador 1 lanza por segunda ocación y no es interceptado por el jugador 2, entonces anota
-  Por ejemplo:
-  >>> score2(1)
-  (30, 0)
-  """
-  j1 = 30
-  j2 = 0
-
-  return j1,j2
-
-def score3(j):
-  """
-  El jugador 1 lanza por tercera ocación y no es interceptado por el jugador 2, entonces anota
-  Por ejemplo:
-  >>> score3(1)
-  (40, 0)
-  """
-  j1 = 40
-  j2 = 0
-
-  return j1,j2
-
-def score4(j):
-  """
-  El jugador 1 lanza por cuarta ocación y no es interceptado por el jugador 2, entonces anota
-  Por ejemplo:
-  >>> score4(1)
-  ('wins!', 'loses')
-  """
-  j1 = 'wins!'
-  j2 = 'loses'
-
-  return j1,j2
-
-def score5(j):
-  """
-  El jugador 1 lanza por primera ocación y es interceptado por el jugador 2, entonces anota jugador 2
-  Por ejemplo:
-  >>> score5(2)
-  (0, 15)
-  """
-  j1 = 0
-  j2 = 15
-
-  return j1,j2
-
-def score6(j):
-  """
-  El jugador 1 lanza por segunda ocación y no es interceptado por el jugador 2, entonces anota jugador 1
-  Por ejemplo:
-  >>> score6(1)
-  (15, 15)
-  """
-  j1 = 15
-  j2 = 15
-
-  return j1,j2
-
-def score7(j):
-  """
-  El jugador 1 lanza por tercera ocación y no es interceptado por el jugador 2, entonces anota jugador 1
-  Por ejemplo:
-  >>> score7(1)
-  (30, 15)
-  """
-  j1 = 30
-  j2 = 15
-
-  return j1,j2
-
-def score8(j):
-  """
-  El jugador 1 lanza por cuarta ocación y es interceptado por el jugador 2, entonces anota jugador 2
-  Por ejemplo:
-  >>> score8(2)
-  (30, 30)
-  """
-  j1 = 30
-  j2 = 30
-
-  return j1,j2
-
-def score9(j):
-  """
-  El jugador 1 lanza por quinta ocación y es interceptado por el jugador 2, entonces anota jugador 2
-  Por ejemplo:
-  >>> score9(2)
-  (30, 40)
-  """
-  j1 = 30
-  j2 = 40
-
-  return j1,j2
-
-def score10(j):
-  """
-  El jugador 1 lanza por sexta ocación y no es interceptado por el jugador 2, entonces anota jugador 1
-  Por ejemplo:
-  >>> score10(1)
-  ('deuce', 'deuce')
-  """
-  j1 = 'deuce'
-  j2 = 'deuce'
-
-  return j1,j2
-
-def score11(j):
-  """
-  El jugador 1 lanza por septima ocación y es interceptado por el jugador 2, entonces anota jugador 2
-  Por ejemplo:
-  >>> score11(2)
-  ('deuce', 'ads')
-  """
-  j1 = 'deuce'
-  j2 = 'ads'
-
-  return j1,j2
-
-def score12(j):
-  """
-  El jugador 1 lanza por septima ocación y no es interceptado por el jugador 2, entonces anota jugador 1
-  Por ejemplo:
-  >>> score12(1)
-  ('ads', 'ads')
-  """
-  j1 = 'ads'
-  j2 = 'ads'
-
-  return j1,j2
-
-def score13(j):
-  """
-  El jugador 1 lanza por septima ocación y no es interceptado por el jugador 2, entonces anota jugador 1
-  Por ejemplo:
-  >>> score13(1)
-  ('wins!', 'ads')
-  """
-  j1 = 'wins!'
-  j2 = 'ads'
-
-  return j1,j2
-
-def score14(j):
-  """
-  El jugador 1 lanza por septima ocación y es interceptado por el jugador 2, entonces anota jugador 2
-  Por ejemplo:
-  >>> score14(2)
-  ('ads', 'wins!')
-  """
-  j1 = 'ads'
-  j2 = 'wins!'
-
-  return j1,j2
+	def score(self):
+		if ((self.j1 < 3 and self.j2 < 3)) or ((self.j1 == 'Adv') and (self.j2 != 'Adv')) or ((self.j1 != 'Adv') and (self.j2 == 'Adv')):
+			return [self.scores[self.j1], self.scores[self.j2]]
+		if ((self.j1 == 3) and (self.j2 == 3)) or ((self.j1 == 4) and (self.j2 == 4)) :
+			return ["Douce"]
+		elif ((self.j1 == 3) and (self.j2 != 3)) or ((self.j1 != 3) and (self.j2 == 3)):	
+			return [self.scores[self.j1], self.scores[self.j2]]
+		if (self.j1 == 5):
+			return ['Game','less']
+		if (self.j2 == 5):
+			return ['less','Game']
 
 if __name__ == "__main__":
     import doctest
